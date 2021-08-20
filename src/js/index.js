3ac442976a754e1c7fbd0ef0c1577ba7da896cdc
Muujii <muujii.tsend@gmail.com>
2021-08-20 13:31:50 -0700

    State and Search COntroller added

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,39 @@
-
+require ("@babel/polyfill");
 import Search from "./model/Search";
 
+/**
+ * Web app tuluv
+ * -Hailtiin query , ur dun
+ * -Tuhain uzuulj bgaa jor
+ * - Likelasen joruud
+ * -ZAhialj bgaa joriin nairlaguud
+ */
 
-let search = new Search('pasta');
+const state = {};
 
-search.doSearch().then(r => console.log("Ur dun : " + r));
+const controlSearch = async () => {
+    // 1) Webees hailtiin tulhvvr ugiig gargaj avna
+    const query = 'pizza';
+
+    
+    if (query) {
+        // 2) SHineer hailtiin objectiig uusgej ogno
+        state.search = new Search(query);
+
+
+        // 3) Hailt hiihed zoriulj delgetsiig UI beltgene
+
+        // 4) Hailtiig gvitsetgene
+        await state.search.doSearch();
+
+        // 5) Hailtiin ur dung delgetsend uzuulne
+        console.log(state.search.result);
+            
+    
+    }
+};
+
+document.querySelector(".search").addEventListener("submit", e => {
+    e.preventDefault();
+    controlSearch();
+});
