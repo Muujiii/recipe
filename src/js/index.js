bb6e4682220d7f2a8c366d611b52af3120727ee3
Muujii <muujii.tsend@gmail.com>
2021-08-25 17:01:56 -0700

    Displey selected 1 recipe into main display

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ import Search from "./model/Search";
 import { elements, renderLoader, clearLoader } from "./view/base";
 import * as searchView from './view/searchView';
 import Recipe from "./model/Recipe";
+import { renderRecipe, clearRecipe} from './view/recipeView';
 
 /**
  * Web app tuluv
@@ -30,7 +31,7 @@ const controlSearch = async () => {
         // 3) Hailt hiihed zoriulj delgetsiig UI beltgene
         searchView.clearSearchQuery();
         searchView.clearSearchResult();
-        renderLoader(elements.searchResultDiv);
+        renderLoader(elements.searchResultDiv); // haij bgaa sumtai icon gargadag
 
         // 4) Hailtiig gvitsetgene
         await state.search.doSearch();
@@ -68,17 +69,21 @@ const controlRecipe = async () => {
     state.recipe = new Recipe(id);
 
     // 3) UI delgetsiig beltgene
+    clearRecipe();
+    renderLoader(elements.recipeDiv); // haij bgaa sumtai icon gargadag
 
 
     // 4) Joroo tataj abchirna
     await state.recipe.getRecipe(); //async fn uchraas promise butsaana. Promise-iig duustal hvleene gevel .then gej bno esvel omno ni await, deerh fn omno async bichij bno
 
     // 5)  Joriig gvitsetgeh hugatsaa bolon ortsiig tootsoolno
+    clearLoader();
     state.recipe.calcTime();
     state.recipe.calcPerPerson();
 
     // 6) Joroo delgetsend gargana
-    console.log(state.recipe);
+    renderRecipe(state.recipe);
 
 ;}
 window.addEventListener('hashchange', controlRecipe);
+window.addEventListener('load', controlRecipe);
